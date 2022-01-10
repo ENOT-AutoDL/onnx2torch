b409d631593832174436a54e1e89dd8d7b52301f
irkjero <pivanovs@mail.ru>
2022-01-10 15:48:34 +0700

    New activations (#8)
    added:
    * hardsigmoid
    * leaky_relu
    
    Co-authored-by: Peter Ivanov <p.ivanov@expasoft.tech>
    Co-authored-by: IvanovP <p.ivanov@expasoft.com>

--- onnx2torch/node_converters/activations.py
+++ onnx2torch/node_converters/activations.py
@@ -1,4 +1,4 @@
-__all__ = ['OnnxExp']
+__all__ = ['OnnxExp', 'OnnxHardSigmoid', 'OnnxSoftmaxV1V11']
 
 import torch
 from torch import nn
@@ -16,6 +16,16 @@ class OnnxExp(nn.Module):
         return torch.exp(input_tensor)
 
 
+class OnnxHardSigmoid(nn.Module):
+    def __init__(self, alpha: float = 0.2, beta: float = 0.5):
+        super().__init__()
+        self.alpha = alpha
+        self.beta = beta
+
+    def forward(self, input_tensor: torch.Tensor) -> torch.Tensor:
+        return torch.clip(self.alpha * input_tensor + self.beta, min=0.0, max=1.0)
+
+
 class OnnxSoftmaxV1V11(nn.Module):
     def __init__(self, axis: int = 1):
         super().__init__()
@@ -27,6 +37,58 @@ class OnnxSoftmaxV1V11(nn.Module):
         result = torch.softmax(result, -1)
 
         return torch.reshape(result, shape)
+
+
+@add_converter(operation_type='Exp', version=6)
+@add_converter(operation_type='Exp', version=13)
+def _(node: OnnxNode, graph: OnnxGraph) -> OperationConverterResult:  # pylint: disable=unused-argument
+    return OperationConverterResult(
+        torch_module=OnnxExp(),
+        onnx_mapping=onnx_mapping_from_node(node=node),
+    )
+
+
+@add_converter(operation_type='HardSigmoid', version=1)
+@add_converter(operation_type='HardSigmoid', version=6)
+def _(node: OnnxNode, graph: OnnxGraph) -> OperationConverterResult:  # pylint: disable=unused-argument
+    alpha = node.attributes.get('alpha', 0.2)
+    beta = node.attributes.get('beta', 0.5)
+
+    return OperationConverterResult(
+        torch_module=OnnxHardSigmoid(alpha=alpha, beta=beta),
+        onnx_mapping=onnx_mapping_from_node(node=node),
+    )
+
+
+@add_converter(operation_type='LeakyRelu', version=1)
+@add_converter(operation_type='LeakyRelu', version=6)
+def _(node: OnnxNode, graph: OnnxGraph) -> OperationConverterResult:  # pylint: disable=unused-argument
+    alpha = node.attributes.get('alpha', 0.01)
+
+    return OperationConverterResult(
+        torch_module=nn.LeakyReLU(negative_slope=alpha),
+        onnx_mapping=onnx_mapping_from_node(node=node),
+    )
+
+
+@add_converter(operation_type='Relu', version=6)
+@add_converter(operation_type='Relu', version=13)
+@add_converter(operation_type='Relu', version=14)
+def _(node: OnnxNode, graph: OnnxGraph) -> OperationConverterResult:   # pylint: disable=unused-argument
+    return OperationConverterResult(
+        torch_module=nn.ReLU(),
+        onnx_mapping=onnx_mapping_from_node(node=node),
+    )
+
+
+@add_converter(operation_type='Sigmoid', version=1)
+@add_converter(operation_type='Sigmoid', version=6)
+@add_converter(operation_type='Sigmoid', version=13)
+def _(node: OnnxNode, graph: OnnxGraph) -> OperationConverterResult:  # pylint: disable=unused-argument
+    return OperationConverterResult(
+        torch_module=nn.Sigmoid(),
+        onnx_mapping=onnx_mapping_from_node(node=node),
+    )
 
 
 @add_converter(operation_type='Softmax', version=1)
@@ -44,35 +106,5 @@ def _(node: OnnxNode, graph: OnnxGraph) -> OperationConverterResult:  # pylint: 
 
     return OperationConverterResult(
         torch_module=torch.nn.Softmax(dim=axis),
-        onnx_mapping=onnx_mapping_from_node(node=node),
-    )
-
-
-@add_converter(operation_type='Sigmoid', version=1)
-@add_converter(operation_type='Sigmoid', version=6)
-@add_converter(operation_type='Sigmoid', version=13)
-def _(node: OnnxNode, graph: OnnxGraph) -> OperationConverterResult:  # pylint: disable=unused-argument
-
-    return OperationConverterResult(
-        torch_module=nn.Sigmoid(),
-        onnx_mapping=onnx_mapping_from_node(node=node),
-    )
-
-
-@add_converter(operation_type='Relu', version=6)
-@add_converter(operation_type='Relu', version=13)
-@add_converter(operation_type='Relu', version=14)
-def _(node: OnnxNode, graph: OnnxGraph) -> OperationConverterResult:   # pylint: disable=unused-argument
-    return OperationConverterResult(
-        torch_module=nn.ReLU(),
-        onnx_mapping=onnx_mapping_from_node(node=node),
-    )
-
-
-@add_converter(operation_type='Exp', version=6)
-@add_converter(operation_type='Exp', version=13)
-def _(node: OnnxNode, graph: OnnxGraph) -> OperationConverterResult:  # pylint: disable=unused-argument
-    return OperationConverterResult(
-        torch_module=OnnxExp(),
         onnx_mapping=onnx_mapping_from_node(node=node),
     )
