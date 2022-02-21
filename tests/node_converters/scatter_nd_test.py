8d0110cffb05c89e402c5ef010e886fc029fa671
irkjero <pivanovs@mail.ru>
2022-02-21 15:49:33 +0700

    fixed indexing in scatter nd
    Co-authored-by: IvanovP <p.ivanov@expasoft.com>

--- tests/node_converters/scatter_nd_test.py
+++ tests/node_converters/scatter_nd_test.py
@@ -31,12 +31,23 @@ def test_scatter_nd() -> None:
         [[8, 7, 6, 5], [4, 3, 2, 1], [1, 2, 3, 4], [5, 6, 7, 8]],
         [[8, 7, 6, 5], [4, 3, 2, 1], [1, 2, 3, 4], [5, 6, 7, 8]]
     ], dtype=np.float32)
+    indices = np.array([[0, 1], [1, 2]], dtype=np.int64)
+    updates = np.array([
+        [5, 6, 7, 8],
+        [1, 2, 3, 4]
+    ], dtype=np.float32)
+
+    _test_scatter_nd(
+        data=data,
+        indices=indices,
+        updates=updates,
+    )
+
     indices = np.array([[0], [2]], dtype=np.int64)
     updates = np.array([
         [[5, 5, 5, 5], [6, 6, 6, 6], [7, 7, 7, 7], [8, 8, 8, 8]],
         [[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [4, 4, 4, 4]]
     ], dtype=np.float32)
-
     _test_scatter_nd(
         data=data,
         indices=indices,
