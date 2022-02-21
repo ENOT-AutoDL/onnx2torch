45be0778b8305c2459ad3b9cf6afc22f19e95291
igor <i.kalgin@expasoft.tech>
2022-02-21 19:53:17 +0700

    simplified split logic

--- onnx2torch/node_converters/split.py
+++ onnx2torch/node_converters/split.py
@@ -1,5 +1,7 @@
 __all__ = ['OnnxSplit', 'OnnxSplit13']
-from typing import Optional, List
+
+from typing import List
+from typing import Optional
 
 import torch
 from torch import nn
@@ -11,45 +13,43 @@ from onnx2torch.utils.common import OperationConverterResult
 from onnx2torch.utils.common import onnx_mapping_from_node
 
 
-def get_split(axis_len: int, split: Optional[torch.Tensor], num_splits: int):
-    if split is None:
-        quotient = axis_len // num_splits
-        remainder = axis_len % num_splits
-        if remainder == 0:
-            split = [quotient] * num_splits
-        else:
-            split = [quotient] * num_splits + [remainder]
-
-    return torch.tensor(split).tolist()
-
-
 class OnnxSplit13(nn.Module):
     def __init__(self, num_splits: int, axis: int = 0):
         super().__init__()
+
         self.axis = axis
         self.num_splits = num_splits
 
-    def forward(self, input_tensor: torch.Tensor, split: Optional[torch.Tensor] = None) -> torch.Tensor:
-        axis_len = input_tensor.shape[self.axis]
-        split = get_split(axis_len, split, self.num_splits)
-        return torch.split(input_tensor, split, self.axis)
+    def forward(
+            self,
+            input_tensor: torch.Tensor,
+            split: Optional[torch.Tensor] = None,
+    ) -> torch.Tensor:
+        if split is None:
+            axis_len = input_tensor.shape[self.axis]
+            split_size_or_sections = axis_len // self.num_splits
+        else:
+            split_size_or_sections = split.tolist()
+
+        return torch.split(input_tensor, split_size_or_sections, dim=self.axis)
 
 
 class OnnxSplit(nn.Module):
     def __init__(self, num_splits: int, axis: int = 0, split: Optional[List[int]] = None):
         super().__init__()
-        self._split = OnnxSplit13(num_splits, axis)
+
         self.axis = axis
+        self.num_splits = num_splits
         self.split = split
 
     def forward(self, input_tensor: torch.Tensor) -> torch.Tensor:
-        return self._split(input_tensor, self.split)
+        if self.split is None:
+            axis_len = input_tensor.shape[self.axis]
+            split_size_or_sections = axis_len // self.num_splits
+        else:
+            split_size_or_sections = self.split
 
-
-class OnnxSplitV1(OnnxSplit):
-
-    def forward(self, input_tensor: torch.Tensor, split: Optional[torch.Tensor] = None) -> torch.Tensor:
-        return self._split(input_tensor, split or self.split)
+        return torch.split(input_tensor, split_size_or_sections, dim=self.axis)
 
 
 @add_converter(operation_type='Split', version=13)
@@ -70,16 +70,5 @@ def _(node: OnnxNode, graph: OnnxGraph) -> OperationConverterResult:
     num_splits = len(node.output_values)
     return OperationConverterResult(
         torch_module=OnnxSplit(axis=axis, split=split, num_splits=num_splits),
-        onnx_mapping=onnx_mapping_from_node(node=node),
-    )
-
-
-@add_converter(operation_type='Split', version=1)
-def _(node: OnnxNode, graph: OnnxGraph) -> OperationConverterResult:
-    axis = node.attributes.get('axis', 0)
-    split = node.attributes.get('split', None)
-    num_splits = len(node.output_values)
-    return OperationConverterResult(
-        torch_module=OnnxSplitV1(axis=axis, split=split, num_splits=num_splits),
         onnx_mapping=onnx_mapping_from_node(node=node),
     )
