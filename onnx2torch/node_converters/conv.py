943b601a1cbc30afd018f1e07e7daccadfa105cb
niobeus <49533965+niobeus@users.noreply.github.com>
2022-04-08 19:49:53 +0700

    Feat convtranspose (#42)
    fixed output padding parameter (ConvTranspose)
    added tests for output padding (ConvTranspose)
    
    Co-authored-by: a.nazdryukhin <a.nazdryukhin@enot.ai>

--- onnx2torch/node_converters/conv.py
+++ onnx2torch/node_converters/conv.py
@@ -42,28 +42,39 @@ def _(node: OnnxNode, graph: OnnxGraph) -> OperationConverterResult:
         raise NotImplementedError(f'Convolution operation with spatial rank == {spatial_rank} is not implemented')
 
     node_attributes = node.attributes
-    kernel_size = node_attributes.get('kernel_shape', weights.shape[2:])
-    stride = node_attributes.get('strides', 1)
-    dilation = node_attributes.get('dilations', 1)
-    groups = node_attributes.get('group', 1)
-
-    padding = onnx_padding_to_torch_padding(
-        node_attributes.get('pads', [0] * spatial_rank * 2),
-        node_attributes.get('auto_pad', 'NOTSET'),
+    common_kwargs = dict(
+        kernel_size=node_attributes.get('kernel_shape', weights.shape[2:]),
+        stride=node_attributes.get('strides', 1),
+        dilation=node_attributes.get('dilations', 1),
+        groups=node_attributes.get('group', 1),
+        padding=onnx_padding_to_torch_padding(
+            node_attributes.get('pads', [0] * spatial_rank * 2),
+            node_attributes.get('auto_pad', 'NOTSET'),
+        ),
+        bias = bias is not None,
     )
 
-    out_channels = weights.shape[0]
-    in_channels = weights.shape[1]*groups
+    if op_type == 'Conv':
+        special_kwargs = dict(
+            out_channels=weights.shape[0],
+            in_channels=weights.shape[1]*common_kwargs['groups'],
+        )
+    elif op_type == 'ConvTranspose':
+        output_padding = node_attributes.get('output_padding', [0] * spatial_rank * 2)
+        if len(output_padding) > 3:
+            output_padding = onnx_padding_to_torch_padding(
+                output_padding,
+                node_attributes.get('auto_pad', 'NOTSET'),
+            )
+        special_kwargs = dict(
+            out_channels=weights.shape[1]*common_kwargs['groups'],
+            in_channels=weights.shape[0],
+            output_padding=output_padding,
+        )
 
     torch_module = conv_class(
-        in_channels=in_channels,
-        out_channels=out_channels,
-        kernel_size=kernel_size,
-        stride=stride,
-        padding=padding,
-        dilation=dilation,
-        groups=groups,
-        bias=bias is not None,
+        **common_kwargs,
+        **special_kwargs,
     )
 
     with torch.no_grad():
