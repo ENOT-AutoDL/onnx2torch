e7bd925cc06e3c61c218c9200a9db2bf26b2c201
Chih-Hsuan Yen <yan12125@gmail.com>
2022-06-24 14:12:54 +0800

    fix: make GlobalAveragePool compatible with PyTorch FX QAT

--- onnx2torch/node_converters/global_average_pool.py
+++ onnx2torch/node_converters/global_average_pool.py
@@ -1,4 +1,6 @@
-__all__ = ['OnnxGlobalAveragePool']
+__all__ = ['OnnxGlobalAveragePool', 'OnnxGlobalAveragePoolWithKnownInputShape']
+
+from typing import List
 
 import torch
 from torch import nn
@@ -8,6 +10,7 @@ from onnx2torch.onnx_graph import OnnxGraph
 from onnx2torch.onnx_node import OnnxNode
 from onnx2torch.utils.common import OnnxToTorchModule
 from onnx2torch.utils.common import OperationConverterResult
+from onnx2torch.utils.common import get_shape_from_value_info
 from onnx2torch.utils.common import onnx_mapping_from_node
 
 
@@ -18,9 +21,27 @@ class OnnxGlobalAveragePool(nn.Module, OnnxToTorchModule):
         return torch.mean(input_tensor, dim=x_dims, keepdim=True)
 
 
+class OnnxGlobalAveragePoolWithKnownInputShape(nn.Module, OnnxToTorchModule):
+
+    def __init__(self, input_shape: List[int]):
+        super().__init__()
+        self.x_dims = list(range(2, len(input_shape)))
+
+    def forward(self, input_tensor: torch.Tensor) -> torch.Tensor:
+        return torch.mean(input_tensor, dim=self.x_dims, keepdim=True)
+
+
 @add_converter(operation_type='GlobalAveragePool', version=1)
 def _(node: OnnxNode, graph: OnnxGraph) -> OperationConverterResult:   # pylint: disable=unused-argument
+    input_value_info = graph.value_info[node.input_values[0]]
+    input_shape = get_shape_from_value_info(input_value_info)
+
+    if input_shape is not None:
+        torch_module = OnnxGlobalAveragePoolWithKnownInputShape(input_shape=input_shape)
+    else:
+        torch_module = OnnxGlobalAveragePool()
+
     return OperationConverterResult(
-        torch_module=OnnxGlobalAveragePool(),
+        torch_module=torch_module,
         onnx_mapping=onnx_mapping_from_node(node=node),
     )
