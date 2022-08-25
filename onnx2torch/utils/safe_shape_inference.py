bc614c997530a79359be3e86a3b92f55a7d47b78
Zhiqiang Wang <zhiqwang@foxmail.com>
2022-08-25 19:10:51 +0800

    Fix type annotation (#88)
    fix: bad type annotation in _shape_inference_by_model_path func

--- onnx2torch/utils/safe_shape_inference.py
+++ onnx2torch/utils/safe_shape_inference.py
@@ -12,7 +12,11 @@ def _is_big_model(model: ModelProto) -> bool:
     return model.ByteSize() / (1024 * 1024 * 1024) > 2.0
 
 
-def _shape_inference_by_model_path(model_path: Union[Path, str], output_path: [Path, str], **kwargs) -> ModelProto:
+def _shape_inference_by_model_path(
+    model_path: Union[Path, str],
+    output_path: Union[Path, str],
+    **kwargs,
+) -> ModelProto:
     model_path = str(Path(model_path).resolve())
     output_path = str(Path(output_path).resolve())
     infer_shapes_path(model_path, output_path=output_path, **kwargs)
