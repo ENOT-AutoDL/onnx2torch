38a56feb672afe14c975478269f79ba78bbbd08c
Monica Thieu <mthieu@emory.edu>
2022-09-06 10:46:26 -0400

    code linted and cleaned accordingly

--- onnx2torch/node_converters/lrn.py
+++ onnx2torch/node_converters/lrn.py
@@ -1,6 +1,5 @@
 __all__ = []
 
-import torch
 from torch import nn
 
 from onnx2torch.node_converters.registry import add_converter
@@ -10,13 +9,12 @@ from onnx2torch.utils.common import OperationConverterResult
 from onnx2torch.utils.common import onnx_mapping_from_node
 
 @add_converter(operation_type='LRN', version=13)
-def _(node: OnnxNode, graph: OnnxGraph) -> OperationConverterResult: # pylint: disable=unused-argument
-    
+def _(node: OnnxNode, graph: OnnxGraph) -> OperationConverterResult:  # pylint: disable=unused-argument
     size = node.attributes.get('size')
     alpha = node.attributes.get('alpha', .0001)
     beta = node.attributes.get('beta', .75)
-    k = node.attributes.get('size', 1)
-     
+    k = node.attributes.get('bias', 1) # pylint: disable=invalid-name
+
     return OperationConverterResult(
         torch_module=nn.LocalResponseNorm(size=size, alpha=alpha, beta=beta, k=k),
         onnx_mapping=onnx_mapping_from_node(node=node),
