09bcd2d5f329b576faac8af8d8b36d53daa08ce1
Mason Ma <masoner6429@gmail.com>
2023-02-16 15:36:32 +0800

    feat: Mod

--- onnx2torch/node_converters/mod.py
+++ onnx2torch/node_converters/mod.py
@@ -14,14 +14,25 @@ from onnx2torch.utils.common import onnx_mapping_from_node
 
 
 class OnnxMod(nn.Module, OnnxToTorchModule):  # pylint: disable=missing-class-docstring
+    def __init__(self, fmod: int):
+        super().__init__()
+        self.fmod = fmod
+
     def forward(self, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:  # pylint: disable=missing-function-docstring
-        return torch.fmod(x, y)
+        if self.fmod == 1:
+            return torch.fmod(x, y)
+        elif self.fmod == 0:
+            return torch.remainder(x, y)
+        else:
+            raise ValueError(f'OnnxMod fom must be 0 or 1, but get {self.fmod}')
 
 
 @add_converter(operation_type='Mod', version=10)
 @add_converter(operation_type='Mod', version=13)
 def _(node: OnnxNode, graph: OnnxGraph) -> OperationConverterResult:  # pylint: disable=unused-argument
+    node_attributes = node.attributes
+    fmod = node_attributes.get('fmod', 0)
     return OperationConverterResult(
-        torch_module=OnnxMod(),
+        torch_module=OnnxMod(fmod=fmod),
         onnx_mapping=onnx_mapping_from_node(node=node),
     )
--- tests/node_converters/mod_test.py
+++ tests/node_converters/mod_test.py
@@ -1,17 +1,25 @@
 import numpy as np
 import onnx
+import pytest
 
 from tests.utils.common import check_onnx_model
 from tests.utils.common import make_model_from_nodes
 
 
-def test_mod() -> None:  # pylint: disable=missing-function-docstring
-    x_variants = [np.array([-4.3, 7.2, 5.0, 4.3, -7.2, 8.0]).astype(np.float32)]
+@pytest.mark.parametrize('fmod_type', [0, 1])
+def test_mod(
+    fmod_type: int,
+) -> None:  # pylint: disable=missing-function-docstring
     x_variants = np.random.randn(12, 1).astype(np.float32)
     y_variants = np.random.randn(12, 1).astype(np.float32)
 
+    if fmod_type == 0:
+        x_variants = np.random.randint(10, 15, size=(12, 1))#.astype(np.uint8)
+        y_variants = np.random.randint(10, 15, size=(12, 1))#.astype(np.uint8)
+
     test_inputs = {'x': x_variants, 'y': y_variants}
-    node = onnx.helper.make_node(op_type='Mod', inputs=['x', 'y'], outputs=['z'], fmod=1)
+
+    node = onnx.helper.make_node(op_type='Mod', inputs=['x', 'y'], outputs=['z'], fmod=fmod_type)
     model = make_model_from_nodes(
         nodes=node,
         initializers={},
