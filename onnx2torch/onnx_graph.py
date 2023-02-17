95a6c1793966e9cbd36aa137dd417446aa7f1e13
Jan Haug <jan@recogni.com>
2023-02-17 14:33:09 +0100

    fix

--- onnx2torch/onnx_graph.py
+++ onnx2torch/onnx_graph.py
@@ -28,7 +28,7 @@ class OnnxGraph:  # pylint: disable=missing-class-docstring
         unique_names = []
         counters = {}
         for node in onnx_graph_proto.node:
-            name = (f'{node.domain}/' + (node.name.replace(".", "/") or {node.op_type})).lstrip("/")
+            name = (f'{node.domain}/' + (node.name.replace(".", "/") or node.op_type)).lstrip("/")
             name_counter = counters.setdefault(name, 0)
             counters[name] += 1
             unique_names.append(f'{name}' + (f'_{name_counter}' if name_counter > 0 else ''))
