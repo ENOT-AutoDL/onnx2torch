b41cb5f138605e782fefbb309b51f68e32d93dc9
Mason Ma <masoner6429@gmail.com>
2023-02-23 16:07:23 +0800

    reformatted code

--- onnx2torch/node_converters/eye.py
+++ onnx2torch/node_converters/eye.py
@@ -3,42 +3,46 @@ __all__ = [
 ]
 
 import torch
+from torch import nn
+
 from onnx2torch.node_converters.registry import add_converter
 from onnx2torch.onnx_graph import OnnxGraph
 from onnx2torch.onnx_node import OnnxNode
 from onnx2torch.utils.common import OnnxToTorchModule, OperationConverterResult, onnx_mapping_from_node
-from torch import nn
 
 
 class OnnxEyeLike(nn.Module, OnnxToTorchModule):  # pylint: disable=missing-docstring
-    def __init__(self, k: int, dtype: torch.dtype = None):
+    def __init__(self, eyelike_k: int, dtype: torch.dtype = None):
         super().__init__()
         self.dtype = dtype
-        self.k = k
+        self.eyelike_k = eyelike_k
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:  # pylint: disable=missing-function-docstring
         if len(x.shape) != 2:
             raise ValueError('OnnxEyeLike only support 2-D tensor')
 
-        n = x.size(dim=0)
-        m = x.size(dim=1)
-        if self.k > n:
-            raise ValueError(f'Error EyeLike Attribute k value, the k value is {self.k}, but x shape is {(n,m)}')
+        input_value_n = x.size(dim=0)
+        input_value_m = x.size(dim=1)
+        if self.eyelike_k > input_value_n:
+            raise ValueError(
+                f'Error EyeLike Attribute k value, the k value is {self.eyelike_k},'
+                'but x shape is {(input_value_n, input_value_m)}'
+            )
 
-        if self.k == 0:
-            return torch.eye(n, m, dtype=self.dtype)
+        if self.eyelike_k == 0:
+            return torch.eye(input_value_n, input_value_m, dtype=self.dtype)
 
-        k_tensor = torch.zeros(n, self.k, dtype=self.dtype)
-        eye_tensor = torch.eye(n, m - self.k, dtype=self.dtype)
+        k_tensor = torch.zeros(input_value_n, self.eyelike_k, dtype=self.dtype)
+        eye_tensor = torch.eye(input_value_n, input_value_m - self.eyelike_k, dtype=self.dtype)
         return torch.concat([k_tensor, eye_tensor], axis=1)
 
 
 @add_converter(operation_type='EyeLike', version=9)
 def _(node: OnnxNode, graph: OnnxGraph) -> OperationConverterResult:  # pylint: disable=unused-argument
     node_attributes = node.attributes
-    k = node_attributes.get('k', 0)
+    eyelike_k = node_attributes.get('k', 0)
     dtype = node_attributes.get('dtype', torch.float32)
     return OperationConverterResult(
-        torch_module=OnnxEyeLike(dtype=dtype, k=k),
+        torch_module=OnnxEyeLike(dtype=dtype, eyelike_k=eyelike_k),
         onnx_mapping=onnx_mapping_from_node(node=node),
     )
--- tests/node_converters/eye_test.py
+++ tests/node_converters/eye_test.py
@@ -7,7 +7,7 @@ from tests.utils.common import check_onnx_model, make_model_from_nodes
 
 
 @pytest.mark.parametrize('dtype', ((None),))
-@pytest.mark.parametrize('k', [0, 1, 2])
+@pytest.mark.parametrize('eyelike_k', [0, 1, 2])
 @pytest.mark.parametrize(
     'input_shapes',
     (
@@ -19,13 +19,13 @@ from tests.utils.common import check_onnx_model, make_model_from_nodes
 def test_eye(  # pylint: disable=missing-function-docstring
     input_shapes: Tuple[int],
     dtype: str,
-    k: int,
+    eyelike_k: int,
 ) -> None:
     input_values = np.random.randint(0, 100, size=input_shapes)
 
     test_inputs = {'x': input_values}
 
-    node = onnx.helper.make_node(op_type='EyeLike', inputs=['x'], outputs=['z'], k=k, dtype=dtype)
+    node = onnx.helper.make_node(op_type='EyeLike', inputs=['x'], outputs=['z'], k=eyelike_k, dtype=dtype)
     model = make_model_from_nodes(
         nodes=node,
         initializers={},
