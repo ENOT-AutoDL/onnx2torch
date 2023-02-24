f6668ed7ab38c4c4a0ddbab1e9e9a29b002c7aa9
Mason Ma <masoner6429@gmail.com>
2023-02-24 13:24:57 +0800

    feat: LayerNorm

--- onnx2torch/node_converters/__init__.py
+++ onnx2torch/node_converters/__init__.py
@@ -21,6 +21,7 @@ from onnx2torch.node_converters.gemm import *
 from onnx2torch.node_converters.global_average_pool import *
 from onnx2torch.node_converters.identity import *
 from onnx2torch.node_converters.instance_norm import *
+from onnx2torch.node_converters.layer_norm import *
 from onnx2torch.node_converters.logical import *
 from onnx2torch.node_converters.lrn import *
 from onnx2torch.node_converters.matmul import *
