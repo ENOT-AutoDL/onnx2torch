f42c464d223e1774123dbd116ce0ae72f3ab3368
Mason Ma <masoner6429@gmail.com>
2023-03-15 14:06:00 +0800

    fix eye issues

--- onnx2torch/node_converters/eye.py
+++ onnx2torch/node_converters/eye.py
@@ -21,21 +21,21 @@ class OnnxEyeLike(nn.Module, OnnxToTorchModule):  # pylint: disable=missing-docs
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:  # pylint: disable=missing-function-docstring
         if len(x.shape) != 2:
-            raise ValueError('OnnxEyeLike only support 2-D tensor')
+            raise ValueError(f'EyeLike only supports 2D tensors, got {len(x.shape)}')
 
-        input_value_n = x.size(dim=0)
-        input_value_m = x.size(dim=1)
-        if self.eyelike_k > input_value_n:
+        size_n = x.size(dim=0)
+        size_m = x.size(dim=1)
+        if self.eyelike_k > size_n:
             raise ValueError(
-                f'Error EyeLike Attribute k value, the k value is {self.eyelike_k},'
-                'but x shape is {(input_value_n, input_value_m)}'
+                f'the eyelike_k value is {self.eyelike_k},'
+                f'but x shape is {(size_n, size_m)}, eyelike_k should be greater than {size_n}'
             )
 
         if self.eyelike_k == 0:
-            return torch.eye(input_value_n, input_value_m, dtype=self.dtype)
+            return torch.eye(size_n, size_m, dtype=self.dtype)
 
-        k_tensor = torch.zeros(input_value_n, self.eyelike_k, dtype=self.dtype)
-        eye_tensor = torch.eye(input_value_n, input_value_m - self.eyelike_k, dtype=self.dtype)
+        k_tensor = torch.zeros(size_n, self.eyelike_k, dtype=self.dtype)
+        eye_tensor = torch.eye(size_n, size_m - self.eyelike_k, dtype=self.dtype)
         return torch.concat([k_tensor, eye_tensor], axis=1)
 
 
